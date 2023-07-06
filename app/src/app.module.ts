1c39b4ccbcf1377c2a71ac4a097a9b51bbde2763
leomozzer <mozzer.ferraz@gmail.com>
2023-07-06 15:48:11 -0300

    Added items

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AzureStorageModule } from './azure-storage/azure-storage.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    AzureStorageModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
