4b04a37a7ee6a84e2eef2b0cb5755fad484b3561
leomozzer <mozzer.ferraz@gmail.com>
2023-07-14 16:44:10 -0300

    Added dynamic rowkey

--- azure-functions/queue-resize-image/index.ts
+++ azure-functions/queue-resize-image/index.ts
@@ -1,16 +1,57 @@
 import { AzureFunction, Context } from "@azure/functions"
-import {BlockBlobClient, BlobServiceClient} from "@azure/storage-blob"
+import { BlockBlobClient, BlobServiceClient } from "@azure/storage-blob"
+import { TableClient, odata } from "@azure/data-tables"
 import * as Jimp from "jimp"
 import * as stream from "stream"
 
 const ONE_MEGABYTE = 1024 * 1024;
 const uploadOptions = { bufferSize: 4 * ONE_MEGABYTE, maxBuffers: 20 };
 
+function getBlobClient(blob: string): BlockBlobClient {
+    try {
+        const blobClientService = BlobServiceClient.fromConnectionString(process.env.AzureWebJobsStorage);
+        const containerClient = blobClientService.getContainerClient(process.env.AzureContainer);
+        const blobClient = containerClient.getBlockBlobClient(blob);
+        return blobClient;
+    } catch (error) {
+        return error;
+    }
+}
+
+function getTableClient() {
+    try {
+        const tableClient = TableClient.fromConnectionString(process.env.AzureWebJobsStorage, process.env.AzureLogTable)
+        return tableClient;
+    } catch (error) {
+        console.log("!!!!!!!!!!!!!")
+        console.log(error)
+        throw error
+    }
+}
+
+async function ReadEntities(partitionKey: string) {
+    try {
+        const tableClient = getTableClient();
+        const listEntities = tableClient.listEntities({
+            queryOptions: { filter: odata`PartitionKey eq ${partitionKey}` }
+        })
+        const iterator = listEntities.byPage({ maxPageSize: 50 })
+        let entities = []
+        for await (const page of iterator) {
+            entities = page;
+            break
+        }
+        return entities;
+    } catch (error) {
+        console.log(error)
+        throw error
+    }
+}
+
 const queueTrigger: AzureFunction = async function (context: Context, myQueueItem: string): Promise<void> {
-    const blobClientService = BlobServiceClient.fromConnectionString(process.env.AzureWebJobsStorage);
-    const containerClient = blobClientService.getContainerClient(process.env.AzureContainer);
-    const blobClient = containerClient.getBlockBlobClient(`original/${myQueueItem}`);
-    const blob = await blobClient.downloadToBuffer() 
+    const blobClient = getBlobClient(`original/${myQueueItem}`)
+    const tableClient = getTableClient();
+    const blob = await blobClient.downloadToBuffer()
     let thumbnail = await Jimp.read(blob);
     thumbnail.resize(100, Jimp.AUTO);
     thumbnail.getBuffer(Jimp.MIME_PNG, async (err, buffer) => {
@@ -22,9 +63,21 @@ const queueTrigger: AzureFunction = async function (context: Context, myQueueIte
                 uploadOptions.bufferSize,
                 uploadOptions.maxBuffers,
                 { blobHTTPHeaders: { blobContentType: "image/jpeg" } });
+            const entities = await ReadEntities('resized')
+            await tableClient.createEntity({
+                partitionKey: "resized",
+                rowKey: String(entities.length + 1),
+                fileName: myQueueItem
+            })
         } catch (err) {
             context.log("error")
             context.log(err.message);
+            const entities = await ReadEntities('error')
+            await tableClient.createEntity({
+                partitionKey: "error",
+                rowKey: String(entities.length + 1),
+                message: err.message
+            })
         }
     })
     context.log('Queue trigger function processed work item', myQueueItem);
